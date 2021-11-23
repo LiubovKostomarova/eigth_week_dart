3a292a4e401b96f17ce804f423ad536190cc14ca
Liubov Kostomarova <liubov.kostomarova@gmail.com>
2021-11-23 17:09:24 +0100

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 //import 'dart:html';
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
@@ -37,7 +39,7 @@ class _ButterfliesListState extends State<ButterfliesList> {
   Butterfly _selectedButterfly;
 
   final List<Butterfly> butterflies = [
-    Butterfly("Adonis",
+    Butterfly ("Adonis",
         "This beautiful species of butterfly is one of the most characteristic of unimproved southern chalk downland, where it can be seen flying low over shortly grazed turf (typically steep, south-facing slopes). The males have brilliant sky-blue wings, while the females are chocolate brown and far less conspicuous. Both sexes have distinctive black lines that enter or cross the white fringes of the wings."),
     Butterfly("Duke of Burgundy",
         "Small and orange and brown, like a tiny fritillary. Undersides of hind wings have rows of white spots. Lives in small colonies on grassland or woodland clearings. This small butterfly frequents scrubby grassland and sunny woodland clearings, typically in very low numbers. The adults rarely visit flowers and most sightings are of the territorial males as they perch on a prominent leaf at the edge of scrub. The females are elusive and spend much of their time resting or flying low to the ground looking for suitable egg-laying sites."),
@@ -51,12 +53,82 @@ class _ButterfliesListState extends State<ButterfliesList> {
 
   int _selectedIndex = -1;
 
+  //final controller = TextEditingController();
+
+  /*void _setText() {
+    setState(() {
+      text = controller.text;
+    });
+  }*/
+
+  String text = "";
+
+  Color _color = const Color(0xFF69E6F7);
+  Color _color2 = const Color(0x66355EE6);
+  Color _changedColor = const Color(0xFFF8507A);
+
+  bool styleOBJ = true;
+
+  bool textValidate = false;
+
+ 
+  bool validateTextField(String text) {
+    if (text.isEmpty) {
+      setState(() {
+        textValidate = true;
+      });
+      return false;
+    }
+    setState(() {
+      textValidate = false;
+    });
+    return true;
+  }
+
+  changeStyle() {
+    setState(() {
+      styleOBJ = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      TextField(
+          decoration: InputDecoration(
+            hintText: 'Enter the butterfly name',
+            errorText: textValidate ? 'Please, enter the name' : null,
+            contentPadding:
+                EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(32.0)),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.lightBlueAccent, width: 1.0),
+              borderRadius: BorderRadius.all(Radius.circular(32.0)),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+              borderRadius: BorderRadius.all(Radius.circular(32.0)),
+            ),
+          ),
+          //controller: controller,
+          onSubmitted: (String str) {
+            setState(
+              () {
+                text = str;
+                /*var index;
+              if (text == butterflies[index].name) {
+                _selectedIndex = index;
+              }*/
+              },
+            );
+            validateTextField(text);
+            changeStyle();
+          }),
       Text(
           _selectedIndex == -1
-              ? "Select one butterfly \u{1F98B} "
+              ? " \u{1F98B} "
               : "\u{1F98B} ${butterflies[_selectedIndex].name}",
           style: TextStyle(fontSize: 30)),
       Expanded(
@@ -68,16 +140,21 @@ class _ButterfliesListState extends State<ButterfliesList> {
                   itemCount: butterflies.length,
                   scrollDirection: Axis.horizontal,
                   itemBuilder: (BuildContext context, int index) => ListTile(
-                      onTap: () {
+                      /*onTap: () {
                         setState(() {
                           // устанавливаем индекс выделенного элемента
                           _selectedIndex = index;
                         });
-                      },
+                      },*/
                       title: Column(children: <Widget>[
-                        const Text('\u{1F98B}'),
-                        Text(butterflies[index].name,
-                            style: const TextStyle(fontSize: 24))
+                        Text(
+                            text != butterflies[index].name &&
+                                    _selectedIndex == -1
+                                ? "There is no such a butterfly"
+                                : "\u{1F98B} ${butterflies[index].name}",
+                            style: styleOBJ
+                                ? TextStyle(fontSize: 24, color: Colors.black)
+                                : TextStyle(fontSize: 26, color: Colors.red))
                       ]),
                       selected: index == _selectedIndex)))),
       SizedBox(
@@ -87,7 +164,7 @@ class _ButterfliesListState extends State<ButterfliesList> {
               _selectedIndex == -1
                   ? ''
                   : butterflies[_selectedIndex].descriptions,
-              style: const TextStyle(fontSize: 20))),
+              style: const TextStyle(fontSize: 20)))
     ]);
   }
 }
